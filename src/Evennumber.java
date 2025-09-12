f12818c506784fc3c5293210b0a7bfd3e74a8760
aboorvaamanivannan <aboorvaamanivannan@gmail.com>
2025-09-12 10:23:04 +0530

    Even Numbers and Odd Numbers

---
+++ src/Evennumber.java
@@ -0,0 +1,11 @@
+public class Even {
+    public static void main(String[] args) {
+        int a=12;
+        if(a % 2==0) {
+            System.out.println("ON");
+        }
+        else {
+            System.out.println("OFF");
+        }
+    }
+}
