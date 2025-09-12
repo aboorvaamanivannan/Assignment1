dc466e18085d65ad448a5233d66912dab62a67ae
aboorvaamanivannan <aboorvaamanivannan@gmail.com>
2025-09-12 10:49:04 +0530

    The Calendar

---
+++ src/year.java
@@ -0,0 +1,11 @@
+public class year {
+    public static void main(String[] args) {
+        int a=2006;
+        if (a%4==0) {
+            System.out.println("It is a leap year");
+        }
+        else{
+            System.out.println("Not a leap year");
+        }
+    }
+}
