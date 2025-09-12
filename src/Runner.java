d7a9de3f246315e43f9c3c149ba47d9f0e85360c
aboorvaamanivannan <aboorvaamanivannan@gmail.com>
2025-09-12 10:28:42 +0530

    Fast Runner

---
+++ src/Runner.java
@@ -0,0 +1,15 @@
+public class Run {
+    public static void main(String[] args) {
+        int a=9;
+        int b=7;
+        if(a>b) {
+            System.out.println("Varun");
+        }
+        else if(a<b) {
+            System.out.println("Arjun");
+        }
+        else {
+            System.out.println("tie");
+        }
+    }
+}
